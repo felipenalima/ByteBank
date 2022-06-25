b5da7698f602bb9c0c937ecd866381dbed2ed8a1
felipenalima <felipena.lima@gmail.com>
2022-06-25 14:00:06 -0300

    feat: add CurrentAccount class

---
+++ ByteBank/CurrentAccount.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class CurrentAccount
+    {
+        public string name;
+        public string account;
+        public int agencyNumber;  
+        public string agencyName;
+        public double balance;
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello World!");
+﻿using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
+using ByteBank;
+
+CurrentAccount account = new CurrentAccount();
+
+account.name = "Felipe Lima";
+account.account = "1234x-X";
+account.agencyName = "Central Agency";
+account.agencyNumber = 23;
+account.balance = 3254.5;
+
+Console.WriteLine("Name: " + account.name);
+Console.WriteLine("Account: " + account.account);
+Console.WriteLine("Agency Name: " + account.agencyName);
+Console.WriteLine("Agency Number: " + account.agencyNumber);
+Console.WriteLine("Balance: " + account.balance);
+Console.ReadLine();
