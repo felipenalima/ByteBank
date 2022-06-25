6ade53fc340f2d4de74ee80a72df1124e78c101d
felipenalima <felipena.lima@gmail.com>
2022-06-25 19:11:24 -0300

    feat: add methods Withdraw, Deposit and Transfer

--- ByteBank/CurrentAccount.cs
+++ ByteBank/CurrentAccount.cs
@@ -12,5 +12,49 @@ namespace ByteBank
         public int agencyNumber;  
         public string agencyName;
         public double balance;
+
+        //Withdraw method, check valor and balance
+        public bool Withdraw(double value)
+        {
+            if(value < 0)
+            {
+                Console.WriteLine("Value can't be less than 0");
+                return false;
+            }
+            else if (balance < value)
+            {
+                Console.WriteLine("Balance less than withdrawal amount");
+                return false;
+            }
+            else 
+            { 
+                Console.WriteLine("Withdrawal value: " + value);
+                balance -= value;
+                return true;
+            }
+        }
+
+        public void Deposit(double value)
+        {
+            balance += value;
+        }
+
+        public bool Transfer(double value, CurrentAccount transferDestiny)
+        {
+            if(value < 0)
+            {
+                return false;
+            }
+            else if(balance < value)
+            {
+                return false; 
+            }
+            else
+            {
+                balance -= value;
+                transferDestiny.balance += value;
+                return true; 
+            }
+        }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -4,6 +4,7 @@ using System.Transactions;
 using ByteBank;
 
 CurrentAccount account = new CurrentAccount();
+CurrentAccount accountTwo = new CurrentAccount();
 
 account.name = "Felipe Lima";
 account.account = "1234x-X";
@@ -11,9 +12,26 @@ account.agencyName = "Central Agency";
 account.agencyNumber = 23;
 account.balance = 3254.5;
 
+accountTwo.name = "Pamela Luz";
+accountTwo.account = "32547-X";
+accountTwo.agencyName = "Central Agency";
+accountTwo.agencyNumber = 2;
+accountTwo.balance = 5000.5;
+
 Console.WriteLine("Name: " + account.name);
 Console.WriteLine("Account: " + account.account);
 Console.WriteLine("Agency Name: " + account.agencyName);
 Console.WriteLine("Agency Number: " + account.agencyNumber);
 Console.WriteLine("Balance: " + account.balance);
+
+Console.WriteLine("Balance before the transfer: " + account.balance);
+account.Withdraw(2000);
+Console.WriteLine("Balance after the transfer: " + account.balance);
+
+account.Deposit(30);
+Console.WriteLine("Balance after the deposit: " + account.balance);
+
+account.Transfer(300, accountTwo);
+System.Console.WriteLine(accountTwo.name + " receveid transfer of " + account.name);
+System.Console.WriteLine("New Balance: " + account.balance);
 Console.ReadLine();
