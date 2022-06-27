845aa20e1bdcdd0346fed72140249a71715cd0c1
felipenalima <felipena.lima@gmail.com>
2022-06-26 21:34:26 -0300

    feat: add Client class

---
+++ ByteBank/Client.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class Client
+    {
+        public string name;
+        public string cpf;
+        public string occupation;
+    }
+}
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -3,35 +3,49 @@ using System.Reflection.Metadata.Ecma335;
 using System.Transactions;
 using ByteBank;
 
-CurrentAccount account = new CurrentAccount();
-CurrentAccount accountTwo = new CurrentAccount();
+// Test for Client class
+Client client = new Client();
+client.name = "Felipe Lima";
+client.cpf = "1234567-40";
+client.occupation = "Embedded Developer";
 
-account.name = "Felipe Lima";
-account.account = "1234x-X";
-account.agencyName = "Central Agency";
-account.agencyNumber = 23;
-account.balance = 3254.5;
+System.Console.WriteLine("Name: " + client.name);
+System.Console.WriteLine("CPF: " + client.cpf);
+System.Console.WriteLine("Occupation: " + client.occupation);
 
-accountTwo.name = "Pamela Luz";
-accountTwo.account = "32547-X";
-accountTwo.agencyName = "Central Agency";
-accountTwo.agencyNumber = 2;
-accountTwo.balance = 5000.5;
 
-Console.WriteLine("Name: " + account.name);
-Console.WriteLine("Account: " + account.account);
-Console.WriteLine("Agency Name: " + account.agencyName);
-Console.WriteLine("Agency Number: " + account.agencyNumber);
-Console.WriteLine("Balance: " + account.balance);
+// Test for CurrentAccount class
 
-Console.WriteLine("Balance before the transfer: " + account.balance);
-account.Withdraw(2000);
-Console.WriteLine("Balance after the transfer: " + account.balance);
+// CurrentAccount account = new CurrentAccount();
+// CurrentAccount accountTwo = new CurrentAccount();
 
-account.Deposit(30);
-Console.WriteLine("Balance after the deposit: " + account.balance);
+// account.name = "Felipe Lima";
+// account.account = "1234x-X";
+// account.agencyName = "Central Agency";
+// account.agencyNumber = 23;
+// account.balance = 3254.5;
 
-account.Transfer(300, accountTwo);
-System.Console.WriteLine(accountTwo.name + " receveid transfer of " + account.name);
-System.Console.WriteLine("New Balance: " + account.balance);
-Console.ReadLine();
+// accountTwo.name = "Pamela Luz";
+// accountTwo.account = "32547-X";
+// accountTwo.agencyName = "Central Agency";
+// accountTwo.agencyNumber = 2;
+// accountTwo.balance = 5000.5;
+
+// Console.WriteLine("Name: " + account.name);
+// Console.WriteLine("Account: " + account.account);
+// Console.WriteLine("Agency Name: " + account.agencyName);
+// Console.WriteLine("Agency Number: " + account.agencyNumber);
+// Console.WriteLine("Balance: " + account.balance);
+
+// Console.WriteLine("Balance before the transfer: " + account.balance);
+// account.Withdraw(2000);
+// Console.WriteLine("Balance after the transfer: " + account.balance);
+
+// account.Deposit(30);
+// Console.WriteLine("Balance after the deposit: " + account.balance);
+
+// account.Transfer(300, accountTwo);
+// System.Console.WriteLine(accountTwo.name + " receveid transfer of " + account.name);
+// System.Console.WriteLine("New Balance: " + account.balance);
+// Console.ReadLine();
+
