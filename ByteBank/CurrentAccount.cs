08b8f8a25f8d37798896ddcbffeeaa6ae3d7cfaa
felipenalima <felipena.lima@gmail.com>
2022-06-28 17:07:44 -0300

    feat: add constructor method

--- ByteBank/CurrentAccount.cs
+++ ByteBank/CurrentAccount.cs
@@ -7,12 +7,54 @@ namespace ByteBank
 {
     public class CurrentAccount
     {
-        public Client Client { get; set; }
-        public string Account { get; set; }
-        public int AgencyNumber { get; set; }
-        public string AgencyName { get; set; }
+        //Fields
+        private string _account;
         private double balance;
+        private int _agencyNumber;
 
+        //Properties
+        public Client Client { get; set; }
+        public string AgencyName { get; set; }
+        public string Account 
+        { 
+            get
+            {
+                return _account;
+            }
+            set 
+            {
+                if(value == null)
+                {
+                    return;
+                }
+                
+                _account = value;
+            } 
+        }
+        public int AgencyNumber 
+        { 
+            get
+            {
+                return _agencyNumber;
+            } 
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+                _agencyNumber = value;
+            } 
+        }
+        public static int TotalAccountsCreated { get; set; }
+
+        //Method Constructor
+        public CurrentAccount(int agencyNumber, string account)
+        {
+            AgencyNumber = agencyNumber;
+            Account = account;
+            TotalAccountsCreated++;
+        }
         //Withdraw method, check valor and balance
         public bool Withdraw(double value)
         {
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -4,6 +4,7 @@ using System.Transactions;
 using ByteBank;
 
 // Test for Client class
+
 Client client = new Client();
 client.Name = "Felipe Lima";
 client.Cpf = "1234567-40";
@@ -16,36 +17,15 @@ System.Console.WriteLine("Occupation: " + client.Occupation);
 
 // Test for CurrentAccount class
 
-CurrentAccount account = new CurrentAccount();
-// CurrentAccount accountTwo = new CurrentAccount();
+CurrentAccount account = new CurrentAccount(23,"1234-X");
+CurrentAccount accountTwo = new CurrentAccount(2,"32547-X");
 
 account.Client = client;
 account.SetBalance(3254.5);
-// account.account = "1234x-X";
-// account.agencyName = "Central Agency";
-// account.agencyNumber = 23;
-
-// accountTwo.name = "Pamela Luz";
-// accountTwo.account = "32547-X";
-// accountTwo.agencyName = "Central Agency";
-// accountTwo.agencyNumber = 2;
-// accountTwo.balance = 5000.5;
 
 Console.WriteLine("Name: " + account.Client.Name);
 Console.WriteLine("Balance: " + account.GetBalance());
-// Console.WriteLine("Account: " + account.account);
-// Console.WriteLine("Agency Name: " + account.agencyName);
-// Console.WriteLine("Agency Number: " + account.agencyNumber);
+Console.WriteLine("Agency Number: " + account.AgencyNumber + " Account: " + account.Account);
+Console.WriteLine("Total Accounts Created: " + CurrentAccount.TotalAccountsCreated);
 
-// Console.WriteLine("Balance before the transfer: " + account.balance);
-// account.Withdraw(2000);
-// Console.WriteLine("Balance after the transfer: " + account.balance);
-
-// account.Deposit(30);
-// Console.WriteLine("Balance after the deposit: " + account.balance);
-
-// account.Transfer(300, accountTwo);
-// System.Console.WriteLine(accountTwo.name + " receveid transfer of " + account.name);
-// System.Console.WriteLine("New Balance: " + account.balance);
-// Console.ReadLine();
 
